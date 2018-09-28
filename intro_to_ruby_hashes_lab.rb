ec0fc233c94d54f3ab46723a6a3eb04547c0ba57
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-28 15:41:48 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,8 @@ def monopoly_with_second_tier
 		monopoly = {}
 		:pieces=4
 		
-	monopoly[:railroads]={:pieces}
-	monopoly[:railroads]=:names=>{}}
-	monopoly[:railroads]={:rent_in_dollars=>{}}
+	monopoly[:railroads]=:pieces
+
 
 end
 
